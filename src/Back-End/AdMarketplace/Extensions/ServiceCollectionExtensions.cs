de8488d5eee2d87db8a6d406a57d042ecfd3aad2
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2026-01-27 21:18:20 +0330

    feat: implement user service and add CORS configuration

--- src/Back-End/AdMarketplace/Extensions/ServiceCollectionExtensions.cs
+++ src/Back-End/AdMarketplace/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@ using System.Text.Json;
 using AdMarketplace.Database;
 using AdMarketplace.Domain.Options;
 using AdMarketplace.Infra.Helpers;
+using AdMarketplace.Infra.Interfaces;
+using AdMarketplace.Infra.Services;
 using FastEndpoints.Security;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,8 @@ public static class ServiceCollectionExtensions
     {
         public IServiceCollection ConfigureServices()
         {
+            services.AddScoped<IUserService, UserService>();
+            
             return services;
         }
         
@@ -50,7 +54,32 @@ public static class ServiceCollectionExtensions
         
             services.Configure<JwtOptions>(
                 configuration.GetSection(JwtOptions.KeyName));
+
+            services.Configure<CorsOptions>(
+                configuration.GetSection(CorsOptions.KeyName));
         
+            return services;
+        }
+
+        public IServiceCollection ConfigureCors(IConfiguration configuration)
+        {
+            var corsOptions = configuration.GetSection(CorsOptions.KeyName).Get<CorsOptions>() ?? new CorsOptions();
+            var allowAnyOrigin = corsOptions.Origins.Count == 0 || corsOptions.Origins.Contains("*");
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(CorsOptions.PolicyName, policy =>
+                {
+                    if (allowAnyOrigin)
+                        policy.AllowAnyOrigin();
+                    else
+                        policy.WithOrigins(corsOptions.Origins.ToArray());
+
+                    policy.AllowAnyMethod();
+                    policy.AllowAnyHeader();
+                });
+            });
+
             return services;
         }
         
--- src/Back-End/AdMarketplace/Program.cs
+++ src/Back-End/AdMarketplace/Program.cs
@@ -1,7 +1,10 @@
 using AdMarketplace;
+using AdMarketplace.Database;
+using AdMarketplace.Domain.Options;
 using AdMarketplace.Extensions;
 using FastEndpoints;
 using FastEndpoints.Swagger;
+using Microsoft.EntityFrameworkCore;
 
 var bld = WebApplication.CreateBuilder();
 bld.Services.AddFastEndpoints();
@@ -12,14 +15,24 @@ bld.Services.SwaggerDocument(options =>
 bld.Services.ConfigureJsonSerializer();
 bld.Services.ConfigureServices();
 bld.Services.ConfigureHelpers();
+bld.Services.ConfigureDbContexts(bld.Configuration);
 bld.Services.ConfigureOptions(bld.Configuration);
 bld.Services.ConfigureAuthentication(bld.Configuration);
+bld.Services.ConfigureCors(bld.Configuration);
 
 var app = bld.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AdMarketDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
 
 app.UseCustomExceptionHandler();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseCors(CorsOptions.PolicyName);
 
 app.UseFastEndpoints();
 
