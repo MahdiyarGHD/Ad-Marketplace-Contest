fad6092b4f2d9197166b97fd0eee5b150466641e
Hamidreza NM <hmdnjf86@gmail.com>
2026-01-29 02:43:45 +0330

    feat: add Categories, add Page Transitions

--- src/Front-End/src/pages/Home.tsx
+++ src/Front-End/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import { memo } from "react";
-import TopBar from "../components/TopBar";
 import PageHeader, { PageHeaderTitle } from "../components/PageHeader";
+import { ChevronRight } from "lucide-react";
+import "./Home.scss";
+import Transition from "../components/Transition";
 
 function Home() {
 	return (
@@ -8,6 +10,25 @@ function Home() {
 			<PageHeader>
 				<PageHeaderTitle>Ad Marketplace</PageHeaderTitle>
 			</PageHeader>
+
+			<div className="Categories Section">
+				<div className="title">Categories</div>
+				<div className="Items">
+					<Transition state eachElement eachElementDelay={20}>
+						<div className="Item">
+							<div className="icon">😂</div>
+							<div className="body">
+								<div className="title">Category 1</div>
+								<div className="subtitle">Description</div>
+							</div>
+							<div className="meta">
+								{/* <div className="count">12</div> */}
+								<ChevronRight />
+							</div>
+						</div>
+					</Transition>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/Front-End/src/pages/MyChannels.tsx
+++ src/Front-End/src/pages/MyChannels.tsx
@@ -4,6 +4,7 @@ import Avatar from "../components/Avatar";
 import { PlusIcon } from "lucide-react";
 import "./MyChannels.scss";
 import { useNavigate } from "react-router-dom";
+import Transition from "../components/Transition";
 
 function MyChannels() {
 	const navigate = useNavigate();
@@ -20,20 +21,25 @@ function MyChannels() {
 			</PageHeader>
 
 			<div className="ChatList">
-				<div className="Item primary" onClick={() => navigate("/add-channel")}>
-					<div className="icon">
-						<PlusIcon />
+				<Transition state eachElement eachElementDelay={50}>
+					<div
+						className="Item primary"
+						onClick={() => navigate("/add-channel")}
+					>
+						<div className="icon">
+							<PlusIcon />
+						</div>
+						<div className="title">Add Your Channel</div>
 					</div>
-					<div className="title">Add Your Channel</div>
-				</div>
-				<div className="ChatItem">
-					<Avatar id="1" title="Channel 1" photo="" />
-					<div className="body">
-						<div className="title">Channel 1</div>
-						<div className="subtitle">Subtitle 1</div>
+					<div className="ChatItem">
+						<Avatar id="1" title="Channel 1" photo="" />
+						<div className="body">
+							<div className="title">Channel 1</div>
+							<div className="subtitle">Subtitle 1</div>
+						</div>
+						<div className="meta">Pending</div>
 					</div>
-					<div className="meta">Pending</div>
-				</div>
+				</Transition>
 			</div>
 
 			<div className="NoChannel"></div>
