addcb156d1e0cc24e3876a82922bbd3c05f03057
HamidrezaNM <hmdnjf86@gmail.com>
2026-02-04 16:37:33 +0330

    feat: add Toast component

--- src/Front-End/src/stores/useUIStore.ts
+++ src/Front-End/src/stores/useUIStore.ts
@@ -1,19 +1,38 @@
 import type { ReactNode } from "react";
 import { create } from "zustand";
 
+type Toast = {
+  id: number;
+  icon: ReactNode;
+  title: string;
+}
+
 type UIState = {
 	topBarTitle?: string;
-	topBarButtons?: ReactNode;
+  topBarButtons?: ReactNode;
+  toasts: Toast[];
 	setTopBarTitle: (value: string) => void;
 	setTopBarButtons: (value: ReactNode) => void;
+	showToast: (toast: Toast) => void;
 };
 
 const useUIStore = create<UIState>((set) => ({
+  toasts: [],
 	setTopBarTitle(value) {
 		set({ topBarTitle: value });
 	},
 	setTopBarButtons(value) {
 		set({ topBarButtons: value });
+  },
+  showToast(toast) {
+    toast.id = Date.now();
+
+    set((state) => ({
+			toasts: [
+        ...state.toasts,
+        toast
+			],
+		}));
 	},
 }));
 
