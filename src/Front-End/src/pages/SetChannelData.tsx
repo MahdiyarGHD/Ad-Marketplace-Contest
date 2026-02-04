61f20a09c84e0c0eecfdf5e11147bbbc91c882df
Hamidreza NM <hmdnjf86@gmail.com>
2026-02-04 03:49:27 +0330

    feat: enhance channel management with dynamic pricing and improved UI components

--- src/Front-End/src/pages/SetChannelData.tsx
+++ src/Front-End/src/pages/SetChannelData.tsx
@@ -1,5 +1,4 @@
 import { memo, useEffect } from "react";
-// import RLottie from "../components/RLottie";
 import { backButton, mainButton } from "@tma.js/sdk-react";
 import { invokeHapticFeedbackImpact } from "../utils/common";
 import { useNavigate } from "react-router";
@@ -7,199 +6,246 @@ import "./SetChannelData.scss";
 import PageHeader, { PageHeaderTitle } from "../components/PageHeader";
 import Avatar from "../components/Avatar";
 import {
-  ChevronDown,
-  ChevronRight,
-  ClockIcon,
-  DollarSignIcon,
-  PlusIcon,
-  SendHorizontalIcon,
-  TagIcon,
+	ChevronDown,
+	ChevronRight,
+	ClockIcon,
+	DollarSignIcon,
+	PlusIcon,
+	SendHorizontalIcon,
+	TagIcon,
 } from "lucide-react";
-import useChannelStore from "../stores/useChannelStore";
+import useChannelStore, {
+	PriceTypes,
+	type Channel,
+} from "../stores/useChannelStore";
 import Menu, { DropdownMenu, MenuItem } from "../components/Menu";
 import TextTransition from "../components/TextTransition";
+import { requestAPI } from "../utils/api";
 
-const PriceTypes = ["Per hour", "Per day", "Per 1000 views"];
 const AdFormats = ["Post"];
 
 function SetChannelData() {
-  const {
-    draftChannel,
-    addDraftChannelPrice,
-    setDraftChannelPriceType,
-    setDraftChannelAdFormat,
-  } = useChannelStore();
+	const {
+		draftChannel,
+		addDraftChannelPrice,
+		setDraftChannelPrice,
+		setDraftChannelPriceType,
+		setDraftChannelAdFormat,
+	} = useChannelStore();
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const onBackButton = () => {
-    navigate("/my-channels");
-  };
+	const onBackButton = () => {
+		navigate("/my-channels");
+	};
 
-  const onSelectCategory = () => {
-    navigate("/select-category");
-  };
+	const onSelectChannel = () => {
+		navigate("/select-channel");
+	};
 
-  const handleSave = () => {
-    // Implement save logic here
-    console.log("Save button clicked");
-  };
+	const onSelectCategory = () => {
+		navigate("/select-category");
+	};
 
-  useEffect(() => {
-    mainButton.setText("Save");
-    mainButton.onClick(handleSave);
-    mainButton.show();
+	const handleSave = async () => {
+		if (!draftChannel.pricing) return;
 
-    backButton.show();
+		const response = await requestAPI(
+			"/api/channels/",
+			{
+				chat_id: draftChannel.chat_id,
+				category_id: draftChannel.category_id,
+				pricings: draftChannel.pricing.map((item) => ({
+					ad_format: item.ad_format + 1,
+					price_type: item.price_type + 1,
+					price_ton: item.price_ton,
+				})),
+			} as Partial<Channel>,
+			"POST",
+		);
 
-    backButton.onClick(onBackButton);
+		if (!response.isError) {
+			navigate("/add-channel/success");
+			invokeHapticFeedbackImpact("medium");
+			setTimeout(() => invokeHapticFeedbackImpact("soft"), 200);
+			setTimeout(() => invokeHapticFeedbackImpact("soft"), 400);
+		}
+		// TODO: Handle error with toast
+	};
 
-    invokeHapticFeedbackImpact("medium");
+	useEffect(() => {
+		mainButton.setText("Save");
 
-    return () => {
-      mainButton.hide();
+		mainButton.show();
 
-      mainButton.offClick(handleSave);
+		backButton.show();
 
-      backButton.hide();
+		backButton.onClick(onBackButton);
 
-      backButton.offClick(onBackButton);
-    };
-  }, []);
+		invokeHapticFeedbackImpact("medium");
 
-  /* const renderSuccess = () => {
-    return (
-      <div className="Placeholder">
-        <div className="Emoji">
-          <RLottie sticker="congrats" autoplay width={120} height={120} />
-        </div>
-        <h2 className="Title">Your Channel Successfully Added</h2>
-        <div className="Instructions"></div>
-      </div>
-    );
-    }; */
+		return () => {
+			mainButton.hide();
 
-  return (
-    <div className="SetChannelData scrollable">
-      <PageHeader>
-        <PageHeaderTitle>Create your influence channel</PageHeaderTitle>
-      </PageHeader>
+			backButton.hide();
 
-      <div className="Section">
-        <div className="Items">
-          <div className="ChatItem">
-            <Avatar id="1" title="Channel 1" photo="" />
-            <div className="body">
-              <div className="title">Channel 1</div>
-              <div className="subtitle">Subtitle 1</div>
-            </div>
-            <div className="meta">
-              <ChevronRight />
-            </div>
-          </div>
-          <div className="Item" onClick={onSelectCategory}>
-            <div className="icon">
-              <TagIcon />
-            </div>
-            <div className="body">
-              <div className="title">
-                {draftChannel?.category?.name || "Select Category..."}
-              </div>
-            </div>
-            <div className="meta">
-              <ChevronRight />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="Section Pricing">
-        <div className="title">Pricing</div>
-        {draftChannel.pricing?.map((item, index) => (
-          <div className="Items" key={index}>
-            <div className="Item">
-              <div className="icon">
-                <DollarSignIcon />
-              </div>
-              <div className="body">
-                <div className="price">
-                  <input type="text" placeholder="Price" />
-                </div>
-              </div>
-              <div className="meta">TON</div>
-            </div>
-            <Menu
-              custom={
-                <div className="Item">
-                  <div className="icon">
-                    <ClockIcon />
-                  </div>
-                  <div className="body">Price type</div>
-                  <div className="meta">
-                    <TextTransition
-                      text={PriceTypes[item.price_type] || "Per hour"}
-                    />
-                    <ChevronDown />
-                  </div>
-                </div>
-              }
-            >
-              <DropdownMenu className="right">
-                {PriceTypes.map((type, i) => (
-                  <MenuItem
-                    key={type}
-                    title={type}
-                    onClick={() => {
-                      console.log("kir");
-                      setDraftChannelPriceType(i, index);
-                    }}
-                  />
-                ))}
-              </DropdownMenu>
-            </Menu>
-            <Menu
-              custom={
-                <div className="Item">
-                  <div className="icon">
-                    <SendHorizontalIcon />
-                  </div>
-                  <div className="body">Ad format</div>
-                  <div className="meta">
-                    <TextTransition
-                      text={AdFormats[item.ad_format] || "Post"}
-                    />
-                    <ChevronDown />
-                  </div>
-                </div>
-              }
-            >
-              <DropdownMenu className="right">
-                {AdFormats.map((format, i) => (
-                  <MenuItem
-                    key={format}
-                    title={format}
-                    onClick={() => setDraftChannelAdFormat(i, index)}
-                  />
-                ))}
-              </DropdownMenu>
-            </Menu>
-          </div>
-        ))}
-      </div>
-      <div className="Section">
-        <div className="Items">
-          <div className="Item primary" onClick={() => addDraftChannelPrice()}>
-            <div className="icon">
-              <PlusIcon />
-            </div>
-            <div className="body">
-              <div className="title">Add Price...</div>
-            </div>
-          </div>
-        </div>
-        <div className="description">Set your price for sponsored content</div>
-      </div>
-    </div>
-  );
+			backButton.offClick(onBackButton);
+		};
+	}, []);
+
+	useEffect(() => {
+		mainButton.onClick(handleSave);
+
+		return () => mainButton.offClick(handleSave);
+	}, [draftChannel]);
+
+	const allPriceTypes = draftChannel.pricing?.map((item) => item.price_type);
+
+	return (
+		<div className="SetChannelData scrollable">
+			<PageHeader>
+				<PageHeaderTitle>Create your influence channel</PageHeaderTitle>
+			</PageHeader>
+
+			<div className="Section">
+				<div className="Items">
+					<div className="ChatItem" onClick={onSelectChannel}>
+						<Avatar
+							id={draftChannel.chat_id ?? ""}
+							title={draftChannel.title ?? "C"} /* photo={draftChannel.photo} */
+						/>
+						<div className="body">
+							<div className="title">
+								{draftChannel.title || "Select Channel..."}
+							</div>
+							{draftChannel.chat_id && (
+								<div className="subtitle">{draftChannel.chat_id ?? ""}</div>
+							)}
+						</div>
+						<div className="meta">
+							<ChevronRight />
+						</div>
+					</div>
+					<div className="Item" onClick={onSelectCategory}>
+						<div className="icon">
+							<TagIcon />
+						</div>
+						<div className="body">
+							<div className="title">
+								{draftChannel?.category?.name || "Select Category..."}
+							</div>
+						</div>
+						<div className="meta">
+							<ChevronRight />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="Section Pricing">
+				<div className="title">Pricing</div>
+				{draftChannel.pricing?.map((item, index) => {
+					const availablePriceType = PriceTypes.filter(
+						(type, i) =>
+							!allPriceTypes!.includes(i) ||
+							type === PriceTypes[item.price_type],
+					);
+
+					return (
+						<div className="Items" key={index}>
+							<div className="Item">
+								<div className="icon">
+									<DollarSignIcon />
+								</div>
+								<div className="body">
+									<div className="price">
+										<input
+											type="text"
+											placeholder="Price"
+											value={item.price_ton}
+											onChange={(e) =>
+												setDraftChannelPrice(Number(e.target.value) || 0, index)
+											}
+										/>
+									</div>
+								</div>
+								<div className="meta">TON</div>
+							</div>
+							<Menu
+								custom={
+									<div className="Item">
+										<div className="icon">
+											<ClockIcon />
+										</div>
+										<div className="body">Price type</div>
+										<div className="meta">
+											<TextTransition
+												text={PriceTypes[item.price_type] || "Per hour"}
+											/>
+											<ChevronDown />
+										</div>
+									</div>
+								}
+							>
+								<DropdownMenu className="right">
+									{availablePriceType.map((type) => (
+										<MenuItem
+											key={type}
+											title={type}
+											onClick={() =>
+												setDraftChannelPriceType(
+													PriceTypes.indexOf(type),
+													index,
+												)
+											}
+										/>
+									))}
+								</DropdownMenu>
+							</Menu>
+							<Menu
+								custom={
+									<div className="Item">
+										<div className="icon">
+											<SendHorizontalIcon />
+										</div>
+										<div className="body">Ad format</div>
+										<div className="meta">
+											<TextTransition
+												text={AdFormats[item.ad_format] || "Post"}
+											/>
+											<ChevronDown />
+										</div>
+									</div>
+								}
+							>
+								<DropdownMenu className="right">
+									{AdFormats.map((format, i) => (
+										<MenuItem
+											key={format}
+											title={format}
+											onClick={() => setDraftChannelAdFormat(i, index)}
+										/>
+									))}
+								</DropdownMenu>
+							</Menu>
+						</div>
+					);
+				})}
+			</div>
+			<div className="Section">
+				<div className="Items">
+					<div className="Item primary" onClick={() => addDraftChannelPrice()}>
+						<div className="icon">
+							<PlusIcon />
+						</div>
+						<div className="body">
+							<div className="title">Add Price...</div>
+						</div>
+					</div>
+				</div>
+				<div className="description">Set your price for sponsored content</div>
+			</div>
+		</div>
+	);
 }
 
 export default memo(SetChannelData);
--- src/Front-End/src/stores/useChannelStore.ts
+++ src/Front-End/src/stores/useChannelStore.ts
@@ -1,105 +1,145 @@
 import { create } from "zustand";
 import { requestAPI } from "../utils/api";
 
+export const PriceTypes = ["Per hour", "Per day", "Per 1000 views"];
+
 export type Channel = {
-  chat_id: number;
-  category_id: number;
-  category?: {
-    id: string;
-    name: string;
-    description: string;
-    icon: string;
-    display_order: number;
-  };
-  title: string;
-  pricing: {
-    ad_format: number;
-    price_type: number;
-    price_ton: number;
-  }[];
+	chat_id: number;
+	category_id: number;
+	category?: {
+		id: string;
+		name: string;
+		description: string;
+		icon: string;
+		display_order: number;
+	};
+	title: string;
+	pricing: {
+		ad_format: number;
+		price_type: number;
+		price_ton: number;
+	}[];
 };
 
 type ChannelState = {
-  myChannels: Channel[];
-  draftChannel: Partial<Channel>;
-  unVerifiedChannels: Channel[];
-  getMyChannels: () => Promise<void>;
-  verifyChannel: () => Promise<void>;
-  setDraftChannel: (channel: Channel) => void;
-  addDraftChannelPrice: () => void;
-  setDraftChannelCategory: (category: any) => void;
-  setDraftChannelPriceType: (priceType: number, index: number) => void;
-  setDraftChannelAdFormat: (adFormat: number, index: number) => void;
+	myChannels: Channel[];
+	draftChannel: Partial<Channel>;
+	unVerifiedChannels: Channel[];
+	getMyChannels: () => Promise<void>;
+	verifyChannel: () => Promise<void>;
+	setUnVerifiedChannels: (channels: Channel[]) => void;
+	setDraftChannel: (channel: Channel) => void;
+	addDraftChannelPrice: () => void;
+	setDraftChannelCategory: (category: any) => void;
+	setDraftChannelPriceType: (priceType: number, index: number) => void;
+	setDraftChannelAdFormat: (adFormat: number, index: number) => void;
+	setDraftChannelPrice: (priceTon: number, index: number) => void;
 };
 
-const useChannelStore = create<ChannelState>((set) => ({
-  myChannels: [],
-  draftChannel: {
-    pricing: [
-      {
-        ad_format: 0,
-        price_type: 0,
-        price_ton: 0,
-      },
-    ],
-  },
-  unVerifiedChannels: [],
-  async getMyChannels() {
-    const response = await requestAPI("/api/channels/my", {}, "GET");
+const useChannelStore = create<ChannelState>((set, get) => ({
+	myChannels: [],
+	draftChannel: {
+		pricing: [
+			{
+				ad_format: 0,
+				price_type: 0,
+				price_ton: 0,
+			},
+		],
+	},
+	unVerifiedChannels: [],
+	async getMyChannels() {
+		const response = await requestAPI("/api/channels/my", {}, "GET");
 
-    set({ myChannels: response.value });
-  },
-  async verifyChannel() {
-    const response = await requestAPI("/api/channels/verify-add", {
-      look_back_seconds: 900,
-    });
+		set({ myChannels: response.value });
+	},
+	async verifyChannel() {
+		const response = await requestAPI("/api/channels/verify-add", {
+			look_back_seconds: 900,
+		});
 
-    console.log(response);
+		console.log(response);
 
-    set((state) => ({
-      myChannels: [...state.myChannels, ...response.value],
-    }));
-  },
-  setDraftChannel(channel) {
-    set({ draftChannel: channel });
-  },
-  setDraftChannelCategory(category: any) {
-    set((state) => ({
-      draftChannel: {
-        ...state.draftChannel,
-        category,
-      },
-    }));
-  },
-  addDraftChannelPrice() {
-    set((state) => ({
-      draftChannel: {
-        ...state.draftChannel,
-        pricing: [
-          ...(state.draftChannel.pricing ?? []),
-          { ad_format: 0, price_type: 0, price_ton: 0 },
-        ],
-      },
-    }));
-  },
-  setDraftChannelPriceType(priceType: number, index: number) {
-    set((state) => ({
-      draftChannel: {
-        ...state.draftChannel,
-        pricing: state.draftChannel.pricing?.map((item, i) =>
-          i === index ? { ...item, price_type: priceType } : item,
-        ),
-      },
-    }));
-  },
-  setDraftChannelAdFormat(adFormat: number, index: number) {
-    set((state) => ({
-      draftChannel: {
-        ...state.draftChannel,
-        adFormat,
-      },
-    }));
-  },
+		set(() => ({
+			unVerifiedChannels: response.value,
+		}));
+	},
+	setUnVerifiedChannels(channels: Channel[]) {
+		set({ unVerifiedChannels: channels });
+	},
+	setDraftChannel(channel) {
+		set((state) => ({
+			draftChannel: {
+				...state.draftChannel,
+				chat_id: channel.chat_id,
+				title: channel.title,
+			},
+		}));
+	},
+	setDraftChannelCategory(category: any) {
+		set((state) => ({
+			draftChannel: {
+				...state.draftChannel,
+				category_id: category.id,
+				category,
+			},
+		}));
+	},
+	addDraftChannelPrice() {
+		const allPriceTypes = get().draftChannel.pricing?.map(
+			(item) => item.price_type,
+		);
+
+		const availablePriceType = PriceTypes.filter(
+			(_, i) => !allPriceTypes!.includes(i),
+		);
+
+		set((state) => ({
+			draftChannel: {
+				...state.draftChannel,
+				pricing: [
+					...(state.draftChannel.pricing ?? []),
+					{
+						ad_format: 0,
+						price_type: PriceTypes.indexOf(availablePriceType[0]),
+						price_ton: 0,
+					},
+				],
+			},
+		}));
+	},
+	setDraftChannelPriceType(priceType: number, index: number) {
+		set((state) => ({
+			draftChannel: {
+				...state.draftChannel,
+				pricing: state.draftChannel.pricing?.map((item, i) =>
+					i === index ? { ...item, price_type: priceType } : item,
+				),
+			},
+		}));
+	},
+	setDraftChannelAdFormat(adFormat: number, index: number) {
+		set((state) => ({
+			draftChannel: {
+				...state.draftChannel,
+				pricing: state.draftChannel.pricing?.map((item, i) =>
+					i === index ? { ...item, ad_format: adFormat } : item,
+				),
+			},
+		}));
+	},
+	setDraftChannelPrice(priceTon: number, index: number) {
+		priceTon = Math.max(0, Math.min(100000, priceTon));
+
+		set((state) => ({
+			draftChannel: {
+				...state.draftChannel,
+				pricing: state.draftChannel.pricing?.map((item, i) =>
+					i === index ? { ...item, price_ton: priceTon } : item,
+				),
+			},
+		}));
+	},
 }));
 
 export default useChannelStore;
