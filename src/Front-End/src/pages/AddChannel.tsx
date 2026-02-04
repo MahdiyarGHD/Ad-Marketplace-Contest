bae3ddbb31ab3d14e6c703a058e427640fad3373
Hamidreza NM <hmdnjf86@gmail.com>
2026-02-04 03:47:59 +0330

    feat: add AddChannel Success page

--- src/Front-End/src/pages/AddChannel.tsx
+++ src/Front-End/src/pages/AddChannel.tsx
@@ -2,134 +2,153 @@ import { useEffect, useState } from "react";
 import { backButton, mainButton, openTelegramLink } from "@tma.js/sdk-react";
 import "./AddChannel.scss";
 import { invokeHapticFeedbackImpact } from "../utils/common";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import PageHeader, { PageHeaderTitle } from "../components/PageHeader";
 import RLottie from "../components/RLottie";
 import useChannelStore from "../stores/useChannelStore";
 
 function AddChannel() {
-  const [waiting, setWaiting] = useState(false);
+	const { status } = useParams();
 
-  const { myChannels, verifyChannel } = useChannelStore();
+	const [currentStatus, setCurrentStatus] = useState(status || "initial"); // initial, waiting, success
 
-  const navigate = useNavigate();
+	const { unVerifiedChannels, verifyChannel, setDraftChannel } =
+		useChannelStore();
 
-  const onBackButton = () => {
-    navigate("/my-channels");
-  };
+	const navigate = useNavigate();
 
-  const onSelectChannel = () => {
-    openTelegramLink(
-      `https://t.me/${import.meta.env.VITE_BOT_USERNAME}?startchannel=true&admin=invite_users+promote_members`,
-    );
+	const onBackButton = () => {
+		navigate("/my-channels");
+	};
 
-    setWaiting(true);
+	const onSelectChannel = () => {
+		openTelegramLink(
+			`https://t.me/${import.meta.env.VITE_BOT_USERNAME}?startchannel=true&admin=invite_users+promote_members`,
+		);
 
-    invokeHapticFeedbackImpact("medium");
+		setCurrentStatus("waiting");
 
-    mainButton.setText("Retry");
-  };
+		invokeHapticFeedbackImpact("medium");
 
-  useEffect(() => {
-    mainButton.setText("Select Channel");
-    mainButton.onClick(onSelectChannel);
-    mainButton.show();
+		mainButton.setText("Retry");
+	};
 
-    backButton.show();
+	useEffect(() => {
+		mainButton.setText("Select Channel");
+		mainButton.onClick(onSelectChannel);
+		mainButton.show();
 
-    backButton.onClick(onBackButton);
+		backButton.show();
 
-    invokeHapticFeedbackImpact("medium");
+		backButton.onClick(onBackButton);
 
-    return () => {
-      mainButton.hide();
+		invokeHapticFeedbackImpact("medium");
 
-      mainButton.offClick(onSelectChannel);
+		return () => {
+			mainButton.hide();
 
-      backButton.hide();
+			mainButton.offClick(onSelectChannel);
 
-      backButton.offClick(onBackButton);
-    };
-  }, []);
+			backButton.hide();
 
-  useEffect(() => {
-    if (waiting) {
-      const interval = setInterval(async () => {
-        await verifyChannel();
-      }, 5000);
+			backButton.offClick(onBackButton);
+		};
+	}, []);
 
-      return () => clearInterval(interval);
-    }
-  }, [waiting]);
+	useEffect(() => {
+		if (currentStatus === "waiting") {
+			const interval = setInterval(async () => {
+				await verifyChannel();
+			}, 5000);
 
-  useEffect(() => {
-    if (myChannels.length > 0) {
-      handleChannelVerified();
-    }
-  }, [myChannels]);
+			return () => clearInterval(interval);
+		}
+	}, [currentStatus]);
 
-  const handleChannelVerified = () => {
-    setWaiting(false);
-    console.log("Channel Verified", myChannels);
-    navigate("/set-channel-data");
-    invokeHapticFeedbackImpact("medium");
-    setTimeout(() => invokeHapticFeedbackImpact("soft"), 100);
-    setTimeout(() => invokeHapticFeedbackImpact("soft"), 200);
-  };
+	useEffect(() => {
+		if (unVerifiedChannels.length > 0) {
+			setDraftChannel(unVerifiedChannels[0]);
+			handleChannelVerified();
+		}
+	}, [unVerifiedChannels]);
 
-  const renderInstructions = () => {
-    return (
-      <div className="Placeholder">
-        <div className="Emoji">
-          <RLottie sticker="bubble" autoplay width={120} height={120} />
-        </div>
-        <h2 className="Title">Add Your Channel</h2>
-        <div className="Instructions">
-          <p className="InstructionText">
-            1. Make sure you are an admin of the channel you want to add.
-          </p>
-          <p className="InstructionText">
-            2. Select the channel from the list.
-          </p>
-          <p className="InstructionText">3. Add this bot to your channel.</p>
-          <p className="InstructionText"> - Tap "Administrators".</p>
-          <p className="InstructionText">
-            {" "}
-            - Tap "Add Admin" and search for our Bot.
-          </p>
-          <p className="InstructionText">
-            {" "}
-            - Grant the necessary permissions and save.
-          </p>
-          <p className="InstructionText">
-            4. You're all set! Start managing your channel ads.
-          </p>
-        </div>
-      </div>
-    );
-  };
+	const handleChannelVerified = () => {
+		console.log("Channel Verified", unVerifiedChannels);
+		navigate("/set-channel-data");
+		invokeHapticFeedbackImpact("medium");
+		setTimeout(() => invokeHapticFeedbackImpact("soft"), 200);
+		setTimeout(() => invokeHapticFeedbackImpact("soft"), 400);
+	};
 
-  const renderWaiting = () => {
-    return (
-      <div className="Placeholder">
-        <div className="Emoji">
-          <RLottie sticker="waiting" autoplay loop width={120} height={120} />
-        </div>
-        <h2 className="Title">Waiting to add bot to your channel</h2>
-        <div className="Instructions"></div>
-      </div>
-    );
-  };
+	const renderInstructions = () => {
+		return (
+			<div className="Placeholder">
+				<div className="Emoji">
+					<RLottie sticker="bubble" autoplay width={120} height={120} />
+				</div>
+				<h2 className="Title">Add Your Channel</h2>
+				<div className="Instructions">
+					<p className="InstructionText">
+						1. Make sure you are an admin of the channel you want to add.
+					</p>
+					<p className="InstructionText">
+						2. Select the channel from the list.
+					</p>
+					<p className="InstructionText">3. Add this bot to your channel.</p>
+					<p className="InstructionText"> - Tap "Administrators".</p>
+					<p className="InstructionText">
+						{" "}
+						- Tap "Add Admin" and search for our Bot.
+					</p>
+					<p className="InstructionText">
+						{" "}
+						- Grant the necessary permissions and save.
+					</p>
+					<p className="InstructionText">
+						4. You're all set! Start managing your channel ads.
+					</p>
+				</div>
+			</div>
+		);
+	};
 
-  return (
-    <div className="AddChannel">
-      <PageHeader>
-        <PageHeaderTitle> </PageHeaderTitle>
-      </PageHeader>
+	const renderWaiting = () => {
+		return (
+			<div className="Placeholder">
+				<div className="Emoji">
+					<RLottie sticker="waiting" autoplay loop width={120} height={120} />
+				</div>
+				<h2 className="Title">Waiting to add bot to your channel</h2>
+				<div className="Instructions"></div>
+			</div>
+		);
+	};
 
-      {waiting ? renderWaiting() : renderInstructions()}
-    </div>
-  );
+	const renderSuccess = () => {
+		return (
+			<div className="Placeholder">
+				<div className="Emoji">
+					<RLottie sticker="congrats" autoplay width={120} height={120} />
+				</div>
+				<h2 className="Title">Your Channel Successfully Added</h2>
+				<div className="Instructions"></div>
+			</div>
+		);
+	};
+
+	return (
+		<div className="AddChannel">
+			<PageHeader>
+				<PageHeaderTitle> </PageHeaderTitle>
+			</PageHeader>
+
+			{currentStatus === "waiting"
+				? renderWaiting()
+				: currentStatus === "success"
+					? renderSuccess()
+					: renderInstructions()}
+		</div>
+	);
 }
 
 export default AddChannel;
