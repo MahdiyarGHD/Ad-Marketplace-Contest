a8ba023422c1360277c2159827e0cf722a615823
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2026-02-08 20:17:27 +0330

    feat: remove GetByCampaign endpoint and related request/response models

--- src/Back-End/AdMarketplace.WebApi/Endpoints/CampaignApplication/GetByCampaign/Request.cs
+++
@@ -1,8 +0,0 @@
-namespace AdMarketplace.Endpoints.CampaignApplication.GetByCampaign;
-
-public class Request
-{
-    public Guid CampaignId { get; set; }
-    public int Skip { get; set; } = 0;
-    public int Take { get; set; } = 20;
-}
