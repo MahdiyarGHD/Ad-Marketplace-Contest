0063a91458e0a3ca40b5a36b4a04b303943bcb6a
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2026-02-08 19:19:02 +0330

    feat: add PostedTextHash column to Deals table

---
+++ src/Back-End/AdMarketplace.Database/Migrations/20260208154530_UpdateModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdMarketplace.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PostedTextHash",
+                schema: "AdMarketplace",
+                table: "Deals",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostedTextHash",
+                schema: "AdMarketplace",
+                table: "Deals");
+        }
+    }
+}
