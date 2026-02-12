40943bf64eab1aec97f787bbbeaeb6d1ef310462
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2026-02-12 23:54:46 +0330

    Add SaveChangesAsync to deal endpoints for proper persistence

--- src/Back-End/AdMarketplace.WebApi/Endpoints/Deal/ApproveDraft/Endpoint.cs
+++ src/Back-End/AdMarketplace.WebApi/Endpoints/Deal/ApproveDraft/Endpoint.cs
@@ -13,6 +13,7 @@ public class Endpoint(
     public override void Configure()
     {
         Post("/api/deals/{Id}/approve");
+        Description(d => d.ClearDefaultAccepts());
     }
 
     public override async Task<ErrorOr<Response>> ExecuteAsync(Request req, CancellationToken ct)
