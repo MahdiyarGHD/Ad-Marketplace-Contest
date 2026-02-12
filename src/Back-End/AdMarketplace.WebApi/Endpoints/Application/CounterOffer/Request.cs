03f021e77949fed7388328080720316a242e31f6
MahdiyarGHD <m.gh.1385.mahdiyar@gmail.com>
2026-02-12 23:51:51 +0330

    Add counter-offer endpoints for campaign applications

---
+++ src/Back-End/AdMarketplace.WebApi/Endpoints/Application/CounterOffer/Request.cs
@@ -0,0 +1,13 @@
+using AdMarketplace.Domain.Types;
+
+namespace AdMarketplace.Endpoints.Application.CounterOffer;
+
+public class Request
+{
+    public Guid Id { get; set; }
+    public required AdFormatType AdFormat { get; set; }
+    public required PriceType PriceType { get; set; }
+    public required decimal PriceTon { get; set; }
+    public DateTimeOffset? PostingTime { get; set; }
+    public string? Message { get; set; }
+}
